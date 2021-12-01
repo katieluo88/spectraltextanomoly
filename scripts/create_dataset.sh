7a55d6e4063e238448d2706371b759aef0c89275
Katie Luo <katie.luo88@gmail.com>
2021-12-01 16:41:30 -0500

    modified to print out maximum word count

--- scripts/create_dataset.sh
+++ scripts/create_dataset.sh
@@ -3,19 +3,19 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task 8                 # Total number of CPU cores requested
 #SBATCH --mem=32G                         # CPU Memory pool for all cores
-#SBATCH --partition=gpu  --gres=gpu:3090:1 # Which queue to run on, and what resources to use
+#SBATCH --partition=kilian  --gres=gpu:1 # Which queue to run on, and what resources to use
                                                # --partition=<queue> - Use the `<queue>` queue
                                                # --gres=gpu:4 - Use 1 GPU of any type
                                                # --gres=gpu:1080ti:1 - Use 1 GTX 1080TI GPU
-#SBATCH --time=50:00:00
+#SBATCH --time=100:00:00
 #SBATCH -o /home/kzl6/slurm/logs/spectral-attack-%j.out                              # Name of stdout output file (%j expands to jobId)
 #SBATCH -e /home/kzl6/slurm/logs/spectral-attack-%j.err                             # Name of stderr output file (%j expands to jobId)
 
 # RECIPE="textfooler"
 RECIPE="clare"
-# MODEL="bert-base-uncased-snli"
+MODEL="bert-base-uncased-snli"
 # MODEL="bert-base-uncased-ag-news"
-MODEL="bert-base-uncased-imdb"
+# MODEL="bert-base-uncased-imdb"
 # MODEL="bert-base-uncased-mr"
 SAVE_PATH="data/${RECIPE}-${MODEL}.csv"
 
